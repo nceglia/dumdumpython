3ddeb68f9ccd21beff61f55abcace04a3d35810d
nceglia <nickceglia@gmail.com>
2020-01-12 21:35:01 -0500

    first assignment

---
+++ camilla/assignment1.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def make_a_list():
+    """
+    Make a list and return it
+    """
+    return []
+
+
+if __name__ == '__main__':
+    alist = make_a_list()
+    assert type(alist) == list
+    print ("PASSED!!! Congratulations.")
